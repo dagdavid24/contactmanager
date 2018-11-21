db243c91ee5d65419af6a789b4a1150aea9bd115
Crixus <dagdavid24@yahoo.com>
2018-11-20 21:35:28 -0500

    Updates

---
+++ precache-manifest.30b0a231ff91e69b928bc0209f060dfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "f9349a8d92c2c18047dd",
+    "url": "/contactmanager/static/js/main.f9349a8d.chunk.js"
+  },
+  {
+    "revision": "d96a08cd9f81c63e94a9",
+    "url": "/contactmanager/static/js/1.d96a08cd.chunk.js"
+  },
+  {
+    "revision": "f9349a8d92c2c18047dd",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "d96a08cd9f81c63e94a9",
+    "url": "/contactmanager/static/css/1.faf162f9.chunk.css"
+  },
+  {
+    "revision": "6182ba186588a59257d27a5684ffdfdf",
+    "url": "/contactmanager/index.html"
+  }
+];
