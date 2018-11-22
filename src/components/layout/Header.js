ea52ce66b8207fabac709e221e3971459eb411df
Crixus <dagdavid24@yahoo.com>
2018-11-22 15:59:04 -0500

    Updated Header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Header = props => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-3 py-0">
-      <div className="containner">
+      <div className="container">
         <a href="/" className="navbar-brand">
           {props.branding}
         </a>
